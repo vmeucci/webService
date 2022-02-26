759088eabcf65202876828340456a5e8e44fcf33
vmeucci <viniciusmeuci@icloud.com>
2022-02-26 19:31:54 -0300

    Service layer and component registration

--- src/main/java/com/vini/webService/resources/UserResource.java
+++ src/main/java/com/vini/webService/resources/UserResource.java
@@ -1,19 +1,34 @@
 package com.vini.webService.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vini.webService.entities.User;
+import com.vini.webService.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User user = new User(1L, "Vinicius", "viniciusmeuci@gmail.com", "11958944940", "12345");
-		return ResponseEntity.ok().body(user);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> userList = service.findAll(); 
+		return ResponseEntity.ok().body(userList);
+	}
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
