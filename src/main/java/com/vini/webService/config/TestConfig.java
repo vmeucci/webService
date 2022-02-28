14b309a42ee3859549670ca0fe6fd7b7f155a061
vmeucci <viniciusmeuci@icloud.com>
2022-02-27 23:28:51 -0300

    Payment entity and association

--- src/main/java/com/vini/webService/config/TestConfig.java
+++ src/main/java/com/vini/webService/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.vini.webService.entities.Category;
 import com.vini.webService.entities.Order;
 import com.vini.webService.entities.OrderItem;
+import com.vini.webService.entities.Payment;
 import com.vini.webService.entities.Product;
 import com.vini.webService.entities.User;
 import com.vini.webService.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
