459a5931c4d384f95e7a015d91ac20a47b2c2b04
vmeucci <viniciusmeuci@icloud.com>
2022-02-28 01:24:31 -0300

    User update

--- src/main/java/com/vini/webService/services/UserService.java
+++ src/main/java/com/vini/webService/services/UserService.java
@@ -11,25 +11,39 @@ import com.vini.webService.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
-		
+
 		Optional<User> obj = userRepository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		userRepository.deleteById(id);
+	}
+
+	@SuppressWarnings("deprecation")
+	public User update(Long id, User obj) {
+		User entity = userRepository.getOne(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+
 	}
 }
