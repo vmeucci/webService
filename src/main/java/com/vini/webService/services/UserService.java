545581a03b2273337cfc765e06ad54890ff19b2d
vmeucci <viniciusmeuci@icloud.com>
2022-03-01 17:13:04 -0300

    Exception handler on findById

--- src/main/java/com/vini/webService/services/UserService.java
+++ src/main/java/com/vini/webService/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.vini.webService.entities.User;
 import com.vini.webService.repositories.UserRepository;
+import com.vini.webService.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	public User findById(Long id) {
 
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
