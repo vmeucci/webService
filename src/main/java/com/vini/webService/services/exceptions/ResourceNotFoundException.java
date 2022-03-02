de40597fb22e8f6fbbaf2fd0432cd01a2938c39a
vmeucci <viniciusmeuci@icloud.com>
2022-03-01 22:12:50 -0300

    Creating dev profile

--- src/main/java/com/vini/webService/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/vini/webService/services/exceptions/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.vini.webService.services.exceptions;
 public class ResourceNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	public ResourceNotFoundException(Object id) {
 		super("Resource not found. Id " + id);
 	}
